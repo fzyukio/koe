14c70dc56d85fdae40f5f8c8fd0b443bf73dffa6
Yukio Fukuzawa <nishinokaze8@gmail.com>
2018-10-03 11:32:30 +1300

    Fix script to make it copy files as well

--- koe/management/commands/convert_tensor_to_datamatrix.py
+++ koe/management/commands/convert_tensor_to_datamatrix.py
@@ -1,19 +1,23 @@
+import os
+import shutil
+
 import numpy as np
 from django.core.management.base import BaseCommand
 
 from koe.models import DataMatrix, DerivedTensorData, FullTensorData, Ordination
 from koe.ts_utils import bytes_to_ndarray
 from koe.ts_utils import get_rawdata_from_binary
+from root.utils import ensure_parent_folder_exists
 
 
 class Command(BaseCommand):
     def handle(self, *args, **options):
 
         tensor_to_dm = {}
-        DataMatrix.objects.all().delete()
         for tensor in FullTensorData.objects.all():
             sids_path = tensor.get_sids_path()
             bytes_path = tensor.get_bytes_path()
+            cols_path = tensor.get_cols_path()
 
             sids = bytes_to_ndarray(sids_path, np.int32)
             data = get_rawdata_from_binary(bytes_path, len(sids))
@@ -23,17 +27,30 @@ class Command(BaseCommand):
                 dm = DataMatrix.objects.create(
                     database=tensor.database,
                     name=tensor.name,
-                    created=tensor.created,
                     features_hash=tensor.features_hash,
                     aggregations_hash=tensor.aggregations_hash,
                     ndims=data.shape[1]
                 )
 
+            dm_sids_path = dm.get_sids_path()
+            dm_bytes_path = dm.get_bytes_path()
+            dm_cols_path = dm.get_cols_path()
+
+            ensure_parent_folder_exists(dm_sids_path)
+
+            shutil.copy(sids_path, dm_sids_path)
+            shutil.copy(bytes_path, dm_bytes_path)
+            shutil.copy(cols_path, dm_cols_path)
+
             tensor_to_dm[tensor] = dm
 
-        Ordination.objects.filter(dm__in=tensor_to_dm.values()).delete()
         for tensor in DerivedTensorData.objects.exclude(dimreduce='none'):
             dm = tensor_to_dm[tensor.full_tensor]
+            sids_path = tensor.full_tensor.get_sids_path()
+            bytes_path = tensor.get_bytes_path()
+
+            if not os.path.exists(bytes_path):
+                bytes_path = tensor.full_tensor.get_bytes_path()
 
             method = tensor.dimreduce
             ndims = tensor.ndims
@@ -41,4 +58,14 @@ class Command(BaseCommand):
                 ndims = int(method[4:])
                 method = 'tsne'
 
-            Ordination.objects.create(dm=dm, method=method, ndims=ndims)
+            ord = Ordination.objects.filter(dm=dm, method=method, ndims=ndims).first()
+            if ord is None:
+                ord = Ordination.objects.create(dm=dm, method=method, ndims=ndims)
+
+            ord_sids_path = ord.get_sids_path()
+            ord_bytes_path = ord.get_bytes_path()
+
+            ensure_parent_folder_exists(ord_sids_path)
+
+            shutil.copy(sids_path, ord_sids_path)
+            shutil.copy(bytes_path, ord_bytes_path)
