e0cb2e01e5c18fd568dad80792760677b08f286a
Yukio Fukuzawa <nishinokaze8@gmail.com>
2019-04-20 17:12:47 +1200

    Replace screenshot by youtube clip

--- assets/js/home-page.js
+++ assets/js/home-page.js
@@ -1,25 +1,35 @@
 /**
- * The screenshot in the landking page might become too small and hangs in the middle of the screen.
- * This function ensures the screenshot is always half the width and anchors at the bottom of the screen
+ * The videoclip in the landking page might become too small and hangs in the middle of the screen.
+ * This function ensures the clip is always half the width and anchors at the bottom of the screen
  */
 function anchorLandingPageScreenshot() {
     let section1 = $('#section-1');
     let section1a = section1.find('#section-1a');
     let section1b = section1.find('#section-1b');
-    let screenshot = section1b.find('img');
-    let imgHeight = screenshot.height();
+    let youtubeClip = section1b.find('#youtube-clip');
+    let clipH = youtubeClip.height();
+    let clipW = youtubeClip.width();
 
-    let imgOriginalWidth = screenshot[0].naturalWidth;
-    let imgOriginalHeight = screenshot[0].naturalHeight;
-    let newImgWidth = imgOriginalWidth * imgHeight / imgOriginalHeight;
+    let section1bW = section1b.width();
+    let section1bH = section1b.height();
 
-    let section1Height = section1.height();
-    let section1aNewHeight = section1Height - imgHeight;
-    section1b.height(imgHeight);
-    section1b.width(newImgWidth);
+    let clipNewW = section1bW;
+    let clipNewH = section1bW * clipH / clipW;
+    if (clipNewH > section1bH) {
+        clipNewW = clipW * section1bH / clipH;
+        clipNewH = section1bH;
+    }
+    else {
+        section1bH = clipNewH;
+    }
+    let section1aNewHeight = section1a.height() + section1b.height() - section1bH;
 
+    youtubeClip.width(clipNewW);
+    youtubeClip.height(clipNewH);
+    section1b.height(section1bH);
     section1a.height(section1aNewHeight);
 }
+
 
 /**
  * Make <button>s function like <a>s
